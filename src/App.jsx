0621c0352124cca74a6fcc87e0699d1579b1197b
Chong Rui Xin <rxchong1123@gmail.com>
2026-05-26 17:23:04 +0800

    react-router-dom, browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,13 @@
-import { useState } from "react";
-import { Card } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
 
 function App() {
-  const apiKey = import.meta.env.VITE_API_KEY;
-  const url = `http://api.openweathermap.org/geo/1.0/zip?zip=43200,MY&appid=${apiKey}`;
-
-  const [data, setData] = useState("");
-
-  const handleGenerateWeather = async () => {
-    try {
-      const geocodeResponse = await fetch(url);
-      const geocodeData = await geocodeResponse.json();
-
-      const coordinates = {
-        lat: geocodeData.lat,
-        lon: geocodeData.lon,
-      };
-
-      const weatherResponse = await fetch(
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=minutely,hourly&appid=${apiKey}`,
-      );
-
-      setData(await weatherResponse.json());
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <>
-      <h1>Generate Weather button</h1>
-      <button onClick={handleGenerateWeather}>Generate</button>
-      {data && data.lat && (
-        <>
-          <Card>
-            <Card.Body>
-              <p>{data.timezone}</p>
-              <p>{data.current.temp}</p>
-              <p>{data.daily[0].summary}</p>
-            </Card.Body>
-          </Card>
-        </>
-      )}
-      {data && data.cod && (
-        <>
-          <Card>
-            <Card.Body>
-              <p>{data.message}</p>
-            </Card.Body>
-          </Card>
-        </>
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
