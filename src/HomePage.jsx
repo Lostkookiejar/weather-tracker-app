5e7963ace879b0cddd436f3977d7c2489729171e
Chong Rui Xin <rxchong1123@gmail.com>
2026-05-27 16:14:15 +0800

    advancedmarker, pin

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,38 @@
 import { useState } from "react";
 import { Card } from "react-bootstrap";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import {
+  AdvancedMarker,
+  APIProvider,
+  Map,
+  Pin,
+} from "@vis.gl/react-google-maps";
+
+const locations = [
+  { key: "operaHouse", location: { lat: -33.8567844, lng: 151.213108 } },
+  { key: "tarongaZoo", location: { lat: -33.8472767, lng: 151.2188164 } },
+  { key: "manlyBeach", location: { lat: -33.8209738, lng: 151.2563253 } },
+  { key: "hyderPark", location: { lat: -33.8690081, lng: 151.2052393 } },
+  { key: "theRocks", location: { lat: -33.8587568, lng: 151.2058246 } },
+  { key: "circularQuay", location: { lat: -33.858761, lng: 151.2055688 } },
+  { key: "harbourBridge", location: { lat: -33.852228, lng: 151.2038374 } },
+  { key: "kingsCross", location: { lat: -33.8737375, lng: 151.222569 } },
+  { key: "botanicGardens", location: { lat: -33.864167, lng: 151.216387 } },
+  { key: "museumOfSydney", location: { lat: -33.8636005, lng: 151.2092542 } },
+  { key: "maritimeMuseum", location: { lat: -33.869395, lng: 151.198648 } },
+  {
+    key: "kingStreetWharf",
+    location: { lat: -33.8665445, lng: 151.1989808 },
+  },
+  { key: "aquarium", location: { lat: -33.869627, lng: 151.202146 } },
+  { key: "darlingHarbour", location: { lat: -33.87488, lng: 151.1987113 } },
+  { key: "barangaroo", location: { lat: -33.8605523, lng: 151.1972205 } },
+];
+
 function HomePage() {
-  const apiKey = import.meta.env.VITE_API_KEY;
   const mapsApiKey = import.meta.env.VITE_MAPS_API_KEY;
+  /*
+  const apiKey = import.meta.env.VITE_API_KEY;
+  
   const url = `http://api.openweathermap.org/geo/1.0/zip?zip=43200,MY&appid=${apiKey}`;
 
   const [data, setData] = useState("");
@@ -27,7 +56,7 @@ function HomePage() {
       console.error(error);
     }
   };
-
+  */
   return (
     <>
       <APIProvider
@@ -37,15 +66,10 @@ function HomePage() {
         <Map
           defaultZoom={13}
           defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
-          onCameraChanged={(ev) =>
-            console.log(
-              "camera changed:",
-              ev.detail.center,
-              "zoom:",
-              ev.detail.zoom,
-            )
-          }
-        />
+          mapId="59684e579f820bc4a08db50c"
+        >
+          <PoiMarkers pois={locations} />
+        </Map>
       </APIProvider>
 
       {/* 
@@ -74,5 +98,21 @@ function HomePage() {
     </>
   );
 }
+
+const PoiMarkers = ({ pois }) => {
+  return (
+    <>
+      {pois.map((poi) => (
+        <AdvancedMarker key={poi.key} position={poi.location}>
+          <Pin
+            background={"#FBBC04"}
+            glyphColor={"#000"}
+            borderColor={"#000"}
+          />
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
 
 export default HomePage;
